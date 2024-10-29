58791f1efe3c180b4c94dfdba4a5a422bb25be1e
Daniel Lip <daniel.lipws@gmail.com>
2024-10-29 16:02:04 +0800

    Additional comments

--- models.py
+++ models.py
@@ -33,16 +33,30 @@ class Event(db.Model):
     
     
 class Ticket(db.Model):
-    """Ticket class contains the details of the ticket."""
-    __tablename__ = 'ticket'
-    ticket_id:Mapped[int] = mapped_column(Integer, primary_key=True)
-    ticket_price_cents:Mapped[int] = mapped_column(Integer, nullable=False)
-    purchase_date:Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.now())
-    seat_category:Mapped[str] = mapped_column(String(50), nullable=False, default='empty')
-    
-    owner_id:Mapped[int] = mapped_column(Integer, ForeignKey('user.user_id'), nullable=False) # FK dependency
-    event_id:Mapped[int] = mapped_column(Integer, ForeignKey('event.event_id'), nullable=False)
+    """Ticket class contains the details of the ticket.
 
+    INSERT detail data sources: \n
+    Blockchain: ticket_id, event_id (take from event name), seat_number, category\n
+    Generated: register_date, status, owner_id"""
+    __tablename__ = 'ticket'
+    ticket_id:Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=False) 
+    '''Ticket Blockchain ID, retrieved during Registration phase.'''
+    ticket_price_cents:Mapped[int] = mapped_column(Integer, nullable=False) # stored as cents, convert to dollar value for display by /100
+    register_date:Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.now()) # date when the ticket was registered into system
+    '''Datetime object generated when ticket is registered into system'''
+    seat_category:Mapped[str] = mapped_column(String(50), nullable=False, default='empty') # pull from BC
+    seat_number:Mapped[int] = mapped_column(Integer, nullable=False) # pull from BC
+    status:Mapped[str] = mapped_column(String(50), nullable=False, default='Not Listed') 
+    '''Status of ticket:\n
+    'Not Listed': Default value if the ticket is not listed on the marketplace\n
+    'Listed': Value when the ticket is currently available for sale on the marketplace\n
+    'Expired': Event that ticket is connected to has concluded '''
+
+    # FK dependencies
+    owner_id:Mapped[int] = mapped_column(Integer, ForeignKey('user.user_id'), nullable=False) # added when ticket is registered, updated when bought
+    event_id:Mapped[int] = mapped_column(Integer, ForeignKey('event.event_id'), nullable=False) # trace event id from the matching event name
+
+    # ORM Relationship Attribute 
     ticket_listing_history:Mapped[list['Ticket_Listing']] = relationship('Ticket_Listing', back_populates='ticket', lazy=True)
     owner:Mapped["User"] = relationship('User', back_populates="tickets_owned", foreign_keys=[owner_id]) # child of Event
     event:Mapped["Event"] = relationship('Event', back_populates="tickets", foreign_keys=[event_id]) # every ticket has an event linked to it
@@ -53,18 +67,28 @@ class Ticket(db.Model):
 
 
 class Ticket_Listing(db.Model):
-    """Tracks the transactions of secondary market ticket sales."""
+    """Tracks the transactions of secondary market ticket sales.\n
+    Connected to Ticket (to be sold) and User (Seller,Buyer) tables"""
+
     __tablename__ = 'ticket_listing'
-    listing_id:Mapped[int] = mapped_column(Integer, primary_key=True)
+    listing_id:Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     sale_price_cents:Mapped[int] = mapped_column(Integer, nullable=False, default='0') # divide this value by 100 when displaying so you don't get weird rounding errors
-    status:Mapped[str] = mapped_column(String(50), nullable=False, default='Available') # status of ticket listing: Available/Sold/Reserved?/Removed
+    status:Mapped[str] = mapped_column(String(50), nullable=False, default='Available') 
+    '''Status of ticket listing.\n
+    Available: Listing is currently available to purchase from and the Ticket's event has not ended yet (No buyer yet)\n
+    Sold: Listing is not available to purchase as a buyer has purchased the listed ticket (Has buyer)\n
+    Expired: Listing did not sell and the event has ended already (No buyer)'''
     listed_on:Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.now()) # tracks when the ticket was put up for sale
     sold_on:Mapped[datetime] = mapped_column(DateTime, nullable=True) # tracks when the listing was sold
 
+    # FK Section
     seller_id:Mapped[int] = mapped_column(Integer, ForeignKey('user.user_id'), nullable=False)
+    '''User ID of the seller who listed the ticket'''
     buyer_id:Mapped[int] = mapped_column(Integer, ForeignKey('user.user_id'), nullable=True)
+    '''User ID of the buyer who purchased the ticket [Nullable]'''
     ticket_id:Mapped[int] = mapped_column(Integer, ForeignKey('ticket.ticket_id'), nullable=True)
 
+    # ORM Relationship
     seller:Mapped["User"] = relationship(foreign_keys=[seller_id], back_populates="ticket_sell_list") 
     buyer:Mapped[Optional["User"]] = relationship(foreign_keys=[buyer_id], back_populates="ticket_buy_list")
     ticket:Mapped["Ticket"] = relationship('Ticket', back_populates='ticket_listing_history', foreign_keys=[ticket_id]) # every ticket listing has one ticket linked to it whose details you can access through here
